9d40acac1a6678687f47dd18647479ee43de6b6a
happytimes121 <drakovish123@gmail.com>
2025-09-22 06:03:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,29 +3,29 @@ const config = require('./settings.json');
 
 function createBot() {
   const bot = mineflayer.createBot({
-    username: config['bot-account'].username,
-    password: config['bot-account'].password,
-    auth: config['bot-account'].type,
+    username: config["bot-account"].username,
+    password: config["bot-account"].password || undefined,
+    auth: config["bot-account"].type,
     host: config.server.ip,
     port: config.server.port,
-    version: config.server.version
+    version: config.server.version,
   });
 
   bot.once('spawn', () => {
-    console.log('\x1b[33m[AFKBot] Bot joined the server\x1b[0m');
+    console.log('[AFKBot] Bot joined the server and is now standing AFK.');
   });
 
   bot.on('end', () => {
-    console.log('[INFO] Bot disconnected. Reconnecting in 10s...');
-    setTimeout(createBot, config.reconnect.delay);
+    console.log('[AFKBot] Bot was disconnected. Reconnecting in 10s...');
+    setTimeout(createBot, 10000);
   });
 
-  bot.on('kicked', reason => {
-    console.log(`\x1b[33m[AFKBot] Kicked. Reason:\n${reason}\x1b[0m`);
+  bot.on('kicked', (reason) => {
+    console.log(`[AFKBot] Kicked from server: ${reason}`);
   });
 
-  bot.on('error', err => {
-    console.log(`\x1b[31m[ERROR] ${err.message}\x1b[0m`);
+  bot.on('error', (err) => {
+    console.error(`[AFKBot] Error: ${err.message}`);
   });
 }
 
