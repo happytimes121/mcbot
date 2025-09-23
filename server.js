e6207f01c7f3aabc15c48c5e51882459114c06ec
happytimes121 <drakovish123@gmail.com>
2025-09-23 06:35:05 -0400

    Delete server.js

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => res.send('🤖 AFK Bot is running!'));
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`🌐 Web server running on port ${port}`));
